6c3abde23e02a0d53378beac8ae7a89262233b5a
Md.Omar Faruq <43860406+faruqiAhmed@users.noreply.github.com>
2025-03-03 22:54:50 +0600

    Initial Commit

---
+++ NavigationExmple/NavigationExmpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExmpleApp.swift
+//  NavigationExmple
+//
+//  Created by Md Omar Faruq on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationExmpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NavigationExmpleTests/NavigationExmpleTests.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationExmpleTests.swift
+//  NavigationExmpleTests
+//
+//  Created by Md Omar Faruq on 3/3/25.
+//
+
+import Testing
+@testable import NavigationExmple
+
+struct NavigationExmpleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
